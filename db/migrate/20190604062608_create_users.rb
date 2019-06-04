29f9817f8e3d7fd0eb312271442cc2b34498f1fe
Vladimir Kochergin <kvn047@gmail.com>
2019-06-04 17:33:29 +1000

    Users

---
+++ db/migrate/20190604062608_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :nickname, null: false, index: {unique: true}
+      t.string :email, null: false, index: {unique: true}
+      t.date :birth_date
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end
